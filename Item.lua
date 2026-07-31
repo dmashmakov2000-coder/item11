648699678c2e8c76ae1070caa89f4d9e65f910d7
dmashmakov2000-coder <dmashmakov2000@gmail.com>
2026-08-01 00:07:49 +0500

    Add files via upload

--- Item.lua
+++ Item.lua
@@ -41,9 +41,9 @@ local logo_path = script_folder .. "\\logo1.png"
 local stats_file_path = script_folder .. "\\ScriptTM_stats.json"
 
 local ITEMS_DB_URL = "https://raw.githubusercontent.com/dmashmakov2000-coder/item11/main/items.json"
-local LOGO_URL = "https://raw.githubusercontent.com/dmashmakov2000-coder/item11/main/logo.png"
+local LOGO_URL = "https://raw.githubusercontent.com/dmashmakov2000-coder/item11/main/logo1.png"
 
-local SCRIPT_VERSION = "0.1.1"
+local SCRIPT_VERSION = "0.1.0"
 local UPDATE_URL = "https://raw.githubusercontent.com/dmashmakov2000-coder/item11/main/Item.lua"
 local CFG_FILENAME = 'Script [TM].ini'
 
